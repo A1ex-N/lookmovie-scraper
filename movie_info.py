f57e14a04590154649331e029f5576d145b04d35
A1ex-N <azander724@gmail.com>
2023-09-17 18:49:30 +1000

    initial commit

---
+++ movie_info.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+from datetime import datetime
+from selectolax.parser import Node
+import calendar
+
+
+@dataclass
+class MovieInfo:
+    title: str
+    year: str | None
+    rating: str | None
+    poster_url: str | None
+    page_url: str | None
+    search_page_num: int
+
+    def __init__(self, node: Node, search_page_num: int):
+        self.title = self.get_title(node)
+        self.page_url = self.get_page_link(node)
+        self.rating = self.get_rating(node)
+        self.poster_url = self.get_image_link(node)
+        self.year = self.get_year(node)
+        self.search_page_num = search_page_num
+
+    def to_tuple(self):
+        return (
+            self.title,
+            self.year,
+            self.rating,
+            self.poster_url,
+            self.page_url,
+            self.search_page_num,
+        )
+
+    @staticmethod
+    def get_title(node: Node) -> str:
+        return node.css_first(".mv-item-infor h6 a").text().strip()
+
+    @staticmethod
+    def get_rating(node: Node) -> str | None:
+        try:
+            return node.css_first(".image__placeholder a .rate span").text().strip()
+        except AttributeError:
+            return None
+
+    @staticmethod
+    def get_year(node: Node) -> str | None:
+        try:
+            return node.css_first(".image__placeholder a .year").text().strip()
+        except AttributeError:
+            return None
+
+    @staticmethod
+    def get_image_link(node: Node) -> str | None:
+        try:
+            return node.css_first(".image__placeholder a img").attributes["data-src"]
+        except AttributeError:
+            return None
+
+    @staticmethod
+    def get_page_link(node: Node) -> str | None:
+        try:
+            return node.css_first(".hvr-inner a").attributes["href"]
+        except AttributeError:
+            return None
+
+
+def to_timezone(dt: datetime):
+    return calendar.timegm(dt.utctimetuple())
---
+++ scraper.py
@@ -0,0 +1,109 @@
+import sqlite3
+import asyncio
+from typing import Coroutine, Any
+
+import httpx
+
+from movie_info import MovieInfo
+from selectolax.parser import HTMLParser, Node
+from pprint import pprint
+from datetime import datetime
+
+
+class Scraper:
+    current_page: int = 0
+    home_page_url: str = "https://www.lookmovie2.to/"
+    base_search_url: str = "https://www.lookmovie2.to/movies/search/page/{page}?q="
+    # session: httpx.AsyncClient
+    start_page: int = 1
+    end_page: int = 0  # 2564
+    start_time = datetime
+    processed_pages: int = 0
+    movie_list_node: Node
+    tasks: list[Coroutine[Any, Any, None]] = []
+    movie_list: list[MovieInfo] = []
+    movie_list_tuple: list[
+        tuple[str, str | None, str | None, str | None, str | None, int]
+    ] = []
+    movie_insert_query: str = (
+        "INSERT INTO movies (title, year, rating, poster_url, page_url, search_page_num) VALUES "
+        "(?, ?, ?, ?, ?, ?)"
+    )
+    database_connection: sqlite3.Connection
+    database_name: str = "movies.db"
+
+    def __init__(self, database_name, start_page=1, end_page=0):
+        self.start_page = start_page
+        self.end_page = end_page
+        self.database_name = database_name
+        self.database_connection = sqlite3.connect(self.database_name)
+        self.cursor = self.database_connection.cursor()
+        # self.session = httpx.AsyncClient()
+
+    async def _get_home_page(self, client: httpx.AsyncClient):
+        return await client.get(self.home_page_url)
+
+    def start_scraping_test(self):
+        html = open("search_page.html").read()
+        self.get_movie_list_node(html)
+        for movie_node in self.movie_list_node.iter():
+            self.movie_list.append(MovieInfo(movie_node, 0))
+        # pprint(self.movie_list)
+        self.dump_scrape_status()
+
+    async def start_scraping(self):
+        self.start_time = datetime.now()
+        async with httpx.AsyncClient() as client:
+            await self._get_home_page(client)
+            for page_num in range(self.start_page, self.end_page + 1):
+                self.tasks.append(self.get_episode(client, page_num))
+            await asyncio.gather(*self.tasks)
+        self.dump_scrape_status()
+
+    async def get_episode(self, client: httpx.AsyncClient, page_num: int):
+        self.current_page = page_num
+        current_page_url = self.base_search_url.format(page=page_num)
+        pprint(f"fetching {current_page_url}")
+        try:
+            current_page_content = await client.get(current_page_url)
+            pprint(f"status code: {current_page_content.status_code}")
+            self.get_movie_list_node(current_page_content.content)
+            for movie_node in self.movie_list_node.iter():
+                self.movie_list.append(MovieInfo(movie_node, self.current_page))
+            self.processed_pages += 1
+        except Exception as e:
+            print("Encountered exception ", e)
+            print(f"Dumping scraped information to {self.database_name} and exiting")
+            self.dump_scrape_status()
+            exit(1)
+
+    def dump_scrape_status(self):
+        for movie in self.movie_list:
+            self.movie_list_tuple.append(movie.to_tuple())
+        self.cursor.executemany(self.movie_insert_query, self.movie_list_tuple)
+        self.database_connection.commit()
+        self.database_connection.close()
+        with open("num_pages_scraped.txt", "w") as f:
+            f.write(str(self.current_page))
+        print(
+            f"scraped {len(self.movie_list)} movies from {self.processed_pages} pages in "
+            + f"{datetime.now() - self.start_time}"
+        )
+        print(f"Dumped movie info to '{self.database_name}'")
+        print("dumped number of scraped pages to num_pages_scraped.txt")
+
+    def get_movie_list_node(self, html: str | bytes) -> Node:
+        tree = HTMLParser(html)
+        self.movie_list_node = tree.css_first(".flex-wrap-movielist")
+        return self.movie_list_node
+
+
+async def main():
+    # scraper = Scraper("test_movies.db")
+    # scraper.start_scraping_test()
+    scraper = Scraper("movies.db", 131, 199)
+    await scraper.start_scraping()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
